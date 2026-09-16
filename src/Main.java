edcf9771d5b939d6a26625f7770aedfa618df4d2
Bholu <sahilsahilc912@gmail.com>
2026-09-16 15:36:58 +0530

    firstJavaCode

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main (String[] args){
+
+        //this is comment
+        /*
+        this
+        is
+        multi-line
+        comment
+         */
+
+        System.out.print("i like pizzas\n");
+        System.out.print("i like biryani\n");
+        System.out.print("i like burger");
+
+    }
+}
