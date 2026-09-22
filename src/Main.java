1d34eca6fac048b827d85f7e3d815913a62e876f
Bholu <sahilsahilc912@gmail.com>
2026-09-22 18:18:24 +0530

    Variabless

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,63 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
 public class Main {
 
     public static void main (String[] args) {
 
-        //this is comment
-        /*
-        this
-        is multi
-        line
-        comment
-         */
+       int age = 20;
+       int quantity = 3;
+       int number = 3;
 
-        System.out.println("I like Pizzas");
-        System.out.println("I like burgers");
+        System.out.println("i have " + quantity + " " + "phones");
+        System.out.print("I am " + age + " years old \n" );
+        System.out.println("How many time you have to go " + " =  " + number );
+        System.out.println("your age is " + age + " and you have " + quantity + "cars");
 
-        System.out.print("I like shawarma\n");
-        System.out.print("I like cheese\n");
 
-        System.out.print("I like biryani");
-        System.out.print("I like chicken");
+        char sign = '@';
+        char name = 'S';
+
+        System.out.println(sign + " is the sign ");
+        System.out.println("my name starts with " + name);
+        System.out.println(sign + " this is my sign and my name starts with " + name);
+
+
+        double price = 12.5;
+        double hours = 1.5;
+        double paid = 1.2;
+
+        System.out.println("You have paid " + price + " ruppees");
+        System.out.println("how many hours do you workout = " + hours );
+        System.out.println("you drink " + paid + " litter of coke");
+        System.out.println("money you have " + paid + " and you spending is " + hours);
+
+
+        String naam = "bholu";
+        String gmail = "sahilsahilc912@gmail.com";
+        String food = "lusania";
+
+        System.out.println("my name is " + naam + " and my gmail is " + gmail + "i like " + food);
+
+
+        boolean he = true;
+        boolean she = false;
+
+        System.out.println("i am a boy and its " + he);
+
+        if(she){
+            System.out.println("i am " + "boy");
+        }
+
+        else {
+            System.out.println("i am " + she);
+        }
+
+
+
+
     }
 }
+
+
+
+
