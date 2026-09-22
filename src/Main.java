be0e7682f4cb1b72c5499cd016eb41b6d3b2be0a
Bholu <sahilsahilc912@gmail.com>
2026-09-22 13:25:55 +0530

    myFirstCode

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,7 @@ public class Main {
 
     public static void main (String[] args){
 
-        //this is comment
-        /*
-        this
-        is
-        multi-line
-        comment
-         */
-
-        System.out.print("i like pizzas\n");
-        System.out.print("i like biryani\n");
-        System.out.print("i like burger");
+        System.out.println("i am bholu");
 
     }
 }
