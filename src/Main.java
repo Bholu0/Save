173874003d1b2b5e7e5401007aeb056b6c6246db
Bholu <sahilsahilc912@gmail.com>
2026-09-25 04:42:44 +0530

    User Inputs

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,85 @@
-import jdk.swing.interop.SwingInterOpUtils;
-
-import java.util.Scanner;
-
-public class Main {
-
-    public static void main (String[] args) {
-
-Scanner scanner = new Scanner (System.in);
-
-
-/*
-scanner.nextLine();
-scanner.nextInt();
-scanner.nextDouble();
-scanner.nextBoolean();
- */
-        System.out.print("Enter my name : ");
-        String name = scanner.nextLine();
-
-        System.out.print("Enter youur age : ");
-        int age = scanner.nextInt();
-
-        System.out.print("Enter your gpa : ");
-        Double gpa = scanner.nextDouble();
-
-
-        System.out.println("My name is " + name + " and my age is " + age + " and gpa is " + gpa);
 
 
 
 
-Scanner anyname = new Scanner (System.in);
-    String okkkk = anyname.nextLine();
-
-    //COMMON ISSUE
-
-        System.out.print("what is your height : ");
-        Double tall = scanner.nextDouble();
-        scanner.nextLine();
-
-        System.out.print("what is your naam : ");
-        String naam = scanner.nextLine();
-
-        System.out.println("my height is " + tall + " and my naam is " + naam);
-
-        /*
-        solution
-        use this method
-        scanner.nextLine();
-         */
-
-        //
-
-    }
-}
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+===
