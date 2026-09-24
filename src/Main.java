01829f40b430329b0d44a009e7f180d9b0ed2c88
Bholu <sahilsahilc912@gmail.com>
2026-09-25 05:05:05 +0530

    User Input

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,56 @@
 import jdk.swing.interop.SwingInterOpUtils;
-
+import java.util.Scanner;
 
 public class Main {
 
     public static void main (String[] args){
 
-        int age = 20;
-        int quantity = 3;
-        int number = 3;
-
-        System.out.println("i have " + quantity + " " + "phones");
-        System.out.print("I am " + age + " years old \n" );
-        System.out.println("How many time you have to go " + " =  " + number );
-        System.out.println("your age is " + age + " and you have " + quantity + "cars");
+        Scanner scanner = new Scanner (System.in);
 
 
-        char sign = '@';
-        char name = 'S';
+/*
+scanner.nextLine();
+scanner.nextInt();
+scanner.nextDouble();
+scanner.nextBoolean();
+ */
+        System.out.print("Enter my name : ");
+        String name = scanner.nextLine();
 
-        System.out.println(sign + " is the sign ");
-        System.out.println("my name starts with " + name);
-        System.out.println("my name starts with " + name);
-        System.out.println(sign + " thiis is my sign and my name starts with " + name);
+        System.out.print("Enter youur age : ");
+        int age = scanner.nextInt();
 
-
-        double price = 12.5;
-        double hours = 1.5;
-        double paid = 1.2;
-
-        System.out.println("You have paid " + price + " ruppees");
-        System.out.println("how many hours do you workout = " + hours );
-        System.out.println("you drink " + paid + " litter of coke");
-        System.out.println("money you have " + paid + " and you spending is " + hours);
+        System.out.print("Enter your gpa : ");
+        Double gpa = scanner.nextDouble();
 
 
-        String naam = "bholu";
-        String gmail = "sahilsahilc912@gmail.com";
-        String food = "lusania";
-
-        System.out.println("my name is " + naam + " and my gmail is " + gmail + "i like " + food);
+        System.out.println("My name is " + name + " and my age is " + age + " and gpa is " + gpa);
 
 
-        boolean he = true;
-        boolean she = false;
 
-        System.out.println("i am a boy and its " + he);
+
+        Scanner anyname = new Scanner (System.in);
+        String ok = anyname.nextLine();
+
+        //COMMON ISSUE
+
+        System.out.print("what is your height : ");
+        Double tall = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.print("what is your naam : ");
+        String naam = scanner.nextLine();
+
+        System.out.println("my height is " + tall + " and my naam is " + naam);
+
+        /*
+        solution
+        use this method
+        scanner.nextLine();
+         */
+
+        //
+
     }
 }
 
