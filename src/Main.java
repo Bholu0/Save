33dfc5c4ee23467f3d350bfee14b2c7e221f252c
Bholu <sahilsahilc912@gmail.com>
2026-09-25 04:59:28 +0530

    Variables

--- src/Main.java
+++ src/Main.java
@@ -3,33 +3,50 @@ import jdk.swing.interop.SwingInterOpUtils;
 
 public class Main {
 
-    public static void main (String[] args) {
+    public static void main (String[] args){
 
-        //this is comment
-        /*
-        this
-        is
-        multi-line
-        comments
-         */
+        int age = 20;
+        int quantity = 3;
+        int number = 3;
 
-        System.out.print("i like pizzas\n");
-        System.out.print("i like biryani\n");
-        System.out.print("i like burger");
+        System.out.println("i have " + quantity + " " + "phones");
+        System.out.print("I am " + age + " years old \n" );
+        System.out.println("How many time you have to go " + " =  " + number );
+        System.out.println("your age is " + age + " and you have " + quantity + "cars");
 
+
+        char sign = '@';
+        char name = 'S';
+
+        System.out.println(sign + " is the sign ");
+        System.out.println("my name start with " + name);
+        System.out.println("my name starts with " + name);
+        System.out.println(sign + " thiis is my sign and my name starts with " + name);
+
+
+        double price = 12.5;
+        double hours = 1.5;
+        double paid = 1.2;
+
+        System.out.println("You have paid " + price + " ruppees");
+        System.out.println("how many hours do you workout = " + hours );
+        System.out.println("you drink " + paid + " litter of coke");
+        System.out.println("money you have " + paid + " and you spending is " + hours);
+
+
+        String naam = "bholu";
+        String gmail = "sahilsahilc912@gmail.com";
+        String food = "lusania";
+
+        System.out.println("my name is " + naam + " and my gmail is " + gmail + "i like " + food);
+
+
+        boolean he = true;
+        boolean she = false;
+
+        System.out.println("i am a boy and its " + he);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
